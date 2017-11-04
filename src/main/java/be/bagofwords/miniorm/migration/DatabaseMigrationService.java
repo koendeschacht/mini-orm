529056e2e8606cd0f0f98e0b7bed7c48d8ee3731
Koen Deschacht <koendeschacht@gmail.com>
2017-11-04 10:09:11 +0100

    Make sure we store intermediate migrations

--- src/main/java/be/bagofwords/miniorm/migration/DatabaseMigrationService.java
+++ src/main/java/be/bagofwords/miniorm/migration/DatabaseMigrationService.java
@@ -29,7 +29,11 @@ public class DatabaseMigrationService implements LifeCycleBean {
 
     @Override
     public void startBean() {
-        runMigrations();
+        try {
+            runMigrations();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to executed migrations.", e);
+        }
     }
 
     @Override
@@ -37,81 +41,90 @@ public class DatabaseMigrationService implements LifeCycleBean {
         //Do nothing
     }
 
-    public void runMigrations() {
-        this.databaseService.execute(connection -> {
-            List<BaseMigration> migrations = collectMigrations();
-            ensureMigrationTablePresent(connection);
-            String version = getVersion(connection);
-            int ind = -1;
-            if (!INITIAL_VERSION.equals(version)) {
-                Log.i("Current version of database migrations is " + version);
-                for (int i = 0; i < migrations.size(); i++) {
-                    if (Objects.equals(migrations.get(i).getId(), version)) {
-                        ind = i;
-                    }
+    public void runMigrations() throws SQLException {
+        List<BaseMigration> migrations = collectMigrations();
+        ensureMigrationTablePresent();
+        String version = getVersion();
+        int ind = -1;
+        BaseMigration lastMigration = null;
+        if (!INITIAL_VERSION.equals(version)) {
+            for (int i = 0; i < migrations.size(); i++) {
+                BaseMigration migration = migrations.get(i);
+                if (Objects.equals(migration.getId(), version)) {
+                    ind = i;
+                    lastMigration = migration;
                 }
-                if (ind == -1) {
-                    String errorMessage;
-                    if (migrations.size() == 0) {
-                        errorMessage = "Last recorded migration had " + version + ", but currently no migrations are found.";
-                    } else {
-                        errorMessage = "Last recorded migration had unknown " + version + ", see the logs for the current list of migrations";
-                        for (BaseMigration migration : migrations) {
-                            Log.i("\tMigration " + migration.getId() + " \"" + migration.getDescription() + "\"");
-                        }
-                    }
-                    throw new IllegalStateException(errorMessage);
-                }
-            } else {
-                Log.i("Starting from zero migrations");
             }
-            int numOfMigrationsToExecute = migrations.size() - (ind + 1);
-            if (numOfMigrationsToExecute > 0) {
-                Log.i("Executing " + numOfMigrationsToExecute + " migrations");
-                for (int j = ind + 1; j < migrations.size(); j++) {
-                    BaseMigration migration = migrations.get(j);
-                    Log.i("Executing migration " + migration.getId() + " \"" + migration.getDescription() + "\"");
-                    migration.execute(connection);
-                    version = migration.getId();
-                    updateVersion(version, connection);
+            if (ind == -1) {
+                String errorMessage;
+                if (migrations.size() == 0) {
+                    errorMessage = "Last recorded migration had " + version + ", but currently no migrations are found.";
+                } else {
+                    errorMessage = "Last recorded migration had unknown " + version + ", see the logs for the current list of migrations";
+                    for (BaseMigration migration : migrations) {
+                        Log.i("\tMigration " + migration.getId() + " \"" + migration.getDescription() + "\"");
+                    }
                 }
+                throw new IllegalStateException(errorMessage);
+            }
+        } else {
+            Log.i("Starting from zero migrations");
+        }
+        int numOfMigrationsToExecute = migrations.size() - (ind + 1);
+        String currentVersionMessage = "Current version of database migrations is " + version;
+        if (lastMigration != null) {
+            currentVersionMessage += " " + lastMigration.getDescription();
+        }
+        Log.i(currentVersionMessage);
+        if (numOfMigrationsToExecute > 0) {
+            Log.i("Executing " + numOfMigrationsToExecute + " migrations");
+            for (int j = ind + 1; j < migrations.size(); j++) {
+                BaseMigration migration = migrations.get(j);
+                Log.i("Executing migration " + migration.getId() + " \"" + migration.getDescription() + "\"");
+                this.databaseService.execute(migration::execute);
+                version = migration.getId();
+                updateVersion(version);
+            }
+        }
+    }
+
+    private void updateVersion(String version) throws SQLException {
+        this.databaseService.execute(connection -> {
+            PreparedStatement statement = connection.prepareStatement("update " + MIGRATION_TABLE + " set version=?");
+            statement.setString(1, version);
+            statement.executeUpdate();
+        });
+    }
+
+    private String getVersion() throws SQLException {
+        return this.databaseService.execute(connection -> {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select version from " + MIGRATION_TABLE);
+            if (resultSet.first()) {
+                return resultSet.getString(1);
             } else {
-                Log.i("Migrations up-to-date");
+                throw new IllegalStateException("The version table " + MIGRATION_TABLE + " does not contain a single row!");
             }
         });
     }
 
-    private void updateVersion(String version, Connection connection) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("update " + MIGRATION_TABLE + " set version=?");
-        statement.setString(1, version);
-        statement.executeUpdate();
-    }
-
-    private String getVersion(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select version from " + MIGRATION_TABLE);
-        if (resultSet.first()) {
-            return resultSet.getString(1);
-        } else {
-            throw new IllegalStateException("The version table " + MIGRATION_TABLE + " does not contain a single row!");
-        }
-    }
-
-    private void ensureMigrationTablePresent(Connection connection) throws SQLException {
-        ResultSet result = connection.createStatement().executeQuery("show tables like '" + MIGRATION_TABLE + "';");
-        if (!result.first()) {
-            //Table does not exist yet
-            Log.i("Table " + MIGRATION_TABLE + " does not exist yet, creating it...");
-            connection.createStatement().execute("create table " + MIGRATION_TABLE + " ( `version` varchar(30) );");
-            insertInitialVersion(connection);
-        }
-        //Check that the version table contains at least one row
-        result.close();
-        result = connection.createStatement().executeQuery("select version from " + MIGRATION_TABLE + ";");
-        if (!result.first()) {
-            insertInitialVersion(connection);
-        }
-        result.close();
+    private void ensureMigrationTablePresent() throws SQLException {
+        this.databaseService.execute(connection -> {
+            ResultSet result = connection.createStatement().executeQuery("show tables like '" + MIGRATION_TABLE + "';");
+            if (!result.first()) {
+                //Table does not exist yet
+                Log.i("Table " + MIGRATION_TABLE + " does not exist yet, creating it...");
+                connection.createStatement().execute("create table " + MIGRATION_TABLE + " ( `version` varchar(30) );");
+                insertInitialVersion(connection);
+            }
+            //Check that the version table contains at least one row
+            result.close();
+            result = connection.createStatement().executeQuery("select version from " + MIGRATION_TABLE + ";");
+            if (!result.first()) {
+                insertInitialVersion(connection);
+            }
+            result.close();
+        });
     }
 
     private void insertInitialVersion(Connection connection) throws SQLException {
